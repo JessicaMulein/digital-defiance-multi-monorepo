fec516a5ef3f28cbf52d918f116aefafed57cd73
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-01 16:22:43 +0000

    add new test

--- digital-defiance/libs/brightchain/src/lib/guid.spec.ts
+++ digital-defiance/libs/brightchain/src/lib/guid.spec.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from 'crypto';
 import GuidV4, { Guid } from './guid';
+import * as uuid from 'uuid';
 
 describe('guid', () => {
   it('should create a new guid', () => {
@@ -12,22 +12,27 @@ describe('guid', () => {
     const uuid2 = GuidV4.fromBigint(uuidBigint);
     expect(uuid).toEqual(uuid2);
   });
-});
-it('should convert uuid to and from base64', () => {
-  const uuid = GuidV4.new();
-  const uuidBase64 = uuid.base64;
-  const uuid2 = GuidV4.fromBase64(uuidBase64);
-  expect(uuid.guid).toEqual(uuid2.guid);
-});
-it('should convert uuid to and from buffer', () => {
-  const uuid = GuidV4.new();
-  const uuidBuffer = uuid.shortBuffer;
-  const uuid2 = GuidV4.fromBuffer(uuidBuffer);
-  expect(uuid).toEqual(uuid2);
-});
-it('should convert uuid to and from string buffer', () => {
-  const uuid = GuidV4.new();
-  const uuidBuffer = uuid.guidSringBuffer;
-  const uuid2 = GuidV4.fromStringBuffer(uuidBuffer);
-  expect(uuid).toEqual(uuid2);
+  it('should convert uuid to and from base64', () => {
+    const uuid = GuidV4.new();
+    const uuidBase64 = uuid.base64;
+    const uuid2 = GuidV4.fromBase64(uuidBase64);
+    expect(uuid.guid).toEqual(uuid2.guid);
+  });
+  it('should convert uuid to and from buffer', () => {
+    const uuid = GuidV4.new();
+    const uuidBuffer = uuid.shortBuffer;
+    const uuid2 = GuidV4.fromBuffer(uuidBuffer);
+    expect(uuid).toEqual(uuid2);
+  });
+  it('should convert uuid to and from string buffer', () => {
+    const uuid = GuidV4.new();
+    const uuidBuffer = uuid.guidSringBuffer;
+    const uuid2 = GuidV4.fromStringBuffer(uuidBuffer);
+    expect(uuid).toEqual(uuid2);
+  });
+  it('should create a GuidV4 from a Guid', () => {gi
+    const uuidStr: Guid = uuid.v4() as Guid;
+    const guidV4 = new GuidV4(uuidStr);
+    expect(guidV4.guid).toEqual(uuidStr);
+  });
 });
