a0a3fb90a77cc6269eb9bb8890f78f2f08886c8b
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-12 12:55:05 -0800

    test with sinon

--- digital-defiance/apps/langex/src/app/shared/settingsManager.spec.ts
+++ digital-defiance/apps/langex/src/app/shared/settingsManager.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { SettingsManager } from './settingsManager';
-
+import * as sinon from 'sinon';
+import * as chrome from 'sinon-chrome';
 
 describe('AppComponent', () => {
+  beforeEach(async () => {
+    chrome.reset();
+  });
   it('should create the manager', () => {
     const settingsManager = new SettingsManager();
     expect(settingsManager).toBeTruthy();
+    // creating a new instance loads the settings
+    sinon.assert.calledOnce(chrome.storage.sync.get);
   });
   it('should be able to save the settings', () => {
     const settingsManager = new SettingsManager();
@@ -15,6 +21,8 @@ describe('AppComponent', () => {
     settingsManager.saveSettings();
     // I guess we can either only check that the call was made or emulate the storage so that the calls work
 
+    sinon.assert.calledOnce(chrome.storage.sync.get);
+    sinon.assert.calledOnce(chrome.storage.sync.set);
     // let records = 0;
     // chrome.storage.sync.get([[SettingsManager.settingsKey]], (items: { [key: string]: any }) => {
     //     records++;
