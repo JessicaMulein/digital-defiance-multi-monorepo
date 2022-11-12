bb1bd664e48140c629649a5c3d87ab1608010f56
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-12 11:41:16 -0800

    yay!

--- digital-defiance/apps/langex/src/app/shared/settings.component.spec.ts
+++ digital-defiance/apps/langex/src/app/shared/settings.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { SettingsComponent } from './settings.component';
 import * as chrome from 'sinon-chrome';
+import { AppSettingsService } from './appSettings.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     (global as any).chrome = chrome;
     await TestBed.configureTestingModule({
       declarations: [SettingsComponent],
+      providers: [AppSettingsService],
     }).compileComponents();
   });
   it('should create the component', () => {
