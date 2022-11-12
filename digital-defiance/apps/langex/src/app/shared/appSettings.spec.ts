62b62df1c87211be81eff1985ad3084938ab50a8
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-12 15:34:03 -0800

    test all of the settings

--- digital-defiance/apps/langex/src/app/shared/appSettings.spec.ts
+++ digital-defiance/apps/langex/src/app/shared/appSettings.spec.ts
@@ -1,14 +1,26 @@
 import AppSettings from "./appSettings";
 import SpeechSources from "./speechSources";
+import PreferredVoiceGender from "./preferredVoiceGender";
+import AudioStorageOption from "./audioStorageOption";
+import { DefaultWordMasteryColors } from "./interfaces.d";
 
 describe('AppSettings', () => {
     it('should create the app settings with the default parameters', () => {
         const settings = new AppSettings();
         expect(settings).toBeTruthy();
+        expect(settings.forvoApiKey).toBe('');
+        expect(settings.forvoApiEnabled).toBe(false);
+        expect(settings.lingvoApiKey).toBe('');
+        expect(settings.lingvoApiEnabled).toBe(false);
+        expect(settings.googleApiKey).toBe('');
+        expect(settings.googleApiEnabled).toBe(false);
+        expect(settings.preferredVoiceGender).toBe(PreferredVoiceGender.Either);
         expect(settings.primaryLanguage).toBe('en');
         expect(settings.primaryLocale).toBe('en-US');
+        expect(settings.storeAudio).toBe(AudioStorageOption.None);
         expect(settings.studiedLanguages).toEqual(['uk', 'ru']);
         expect(settings.speechSources).toEqual([SpeechSources.WebSpeechAPI]);
+        expect(settings.wordMasteryColors).toEqual(DefaultWordMasteryColors);
     });
     it('should create the app settings with the specified parameters', () => {
         const settings = new AppSettings('ukX', 'uk-UAX', ['enX', 'ruX'], [SpeechSources.ForvoAPI, SpeechSources.WebSpeechAPI]);
