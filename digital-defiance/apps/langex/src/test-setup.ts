813300f02ab4defc422e25c2d7cb89bebf0143db
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-12 11:43:38 -0800

    cut out unneeded lines

--- digital-defiance/apps/langex/src/test-setup.ts
+++ digital-defiance/apps/langex/src/test-setup.ts
@@ -1,7 +1,5 @@
 import 'jest-preset-angular/setup-jest';
 import * as chrome from 'sinon-chrome';
 
-Object.assign(global, chrome);
-Object.assign(window, chrome);
 (global as any).chrome = chrome;
 (window as any).chrome = chrome;
