d6840d1f38a37262947a29f595dcc651312aec1d
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-13 08:28:21 -0800

    refactor to function

--- digital-defiance/apps/langex/src/app/shared/settingsManager.spec.ts
+++ digital-defiance/apps/langex/src/app/shared/settingsManager.spec.ts
@@ -8,6 +8,37 @@ import PreferredVoiceGender from './preferredVoiceGender';
 import AudioStorageOption from './audioStorageOption';
 import AppSettings from './appSettings';
 
+function makeExpectedSettings(
+  changeKey: string,
+  changeValue: any
+): { [key: string]: any } {
+  const expectedSettings: ISettings = {
+    lingvoApiKey: '',
+    lingvoApiEnabled: false,
+    forvoApiKey: '',
+    forvoApiEnabled: false,
+    googleApiKey: '',
+    googleApiEnabled: false,
+    preferredVoiceGender: PreferredVoiceGender.Either,
+    primaryLanguage: 'en',
+    primaryLocale: 'en-US',
+    storeAudio: AudioStorageOption.None,
+    studiedLanguages: ['uk', 'ru'],
+    speechSources: [SpeechSources.WebSpeechAPI],
+    wordMasteryColors: DefaultWordMasteryColors,
+  };
+  // ensure settings has the changeKey
+  if (!(changeKey in expectedSettings)) {
+    throw new Error(`Invalid changeKey: ${changeKey}`);
+  }
+  (expectedSettings as any)[changeKey] = changeValue;
+  const expectedSettingsObject: { [key: string]: any } = {};
+  expectedSettingsObject[SettingsManager.settingsKey] = JSON.stringify(
+    expectedSettings as AppSettings
+  );
+  return expectedSettingsObject;
+}
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     chrome.reset();
@@ -18,8 +49,26 @@ describe('AppComponent', () => {
     // creating a new instance loads the settings
     sinon.assert.calledOnce(chrome.storage.sync.get);
   });
+  it('should load the correct settings from the storage', () => {
+    // force chrome.storage.sync.get to return the correct value
+    const updatedForvoApiKey = 'forvoApiKeyTest';
+    const expectedSettings = makeExpectedSettings(
+      'forvoApiKey',
+      updatedForvoApiKey
+    );
+    chrome.storage.sync.get.yields(expectedSettings);
+
+    const settingsManager = new SettingsManager();
+    expect(settingsManager).toBeTruthy();
+    expect(settingsManager.Settings.forvoApiKey).toBe(updatedForvoApiKey);
+  });
   it('should be able to save the settings', () => {
-    const updatedForvoApiKey = 'updatedForvoApiKey';
+    const updatedForvoApiKey = 'forvoApiKeyTest';
+    const expectedSettings = makeExpectedSettings(
+      'forvoApiKey',
+      updatedForvoApiKey
+    );
+
     const settingsManager = new SettingsManager();
     expect(settingsManager).toBeTruthy();
     expect(settingsManager.Settings.forvoApiKey).toBe('');
@@ -29,49 +78,6 @@ describe('AppComponent', () => {
     sinon.assert.calledOnce(chrome.storage.sync.get);
     sinon.assert.calledOnce(chrome.storage.sync.set);
 
-    const expectedSettings: ISettings = {
-      lingvoApiKey: '',
-      lingvoApiEnabled: false,
-      forvoApiKey: updatedForvoApiKey,
-      forvoApiEnabled: false,
-      googleApiKey: '',
-      googleApiEnabled: false,
-      preferredVoiceGender: PreferredVoiceGender.Either,
-      primaryLanguage: 'en',
-      primaryLocale: 'en-US',
-      storeAudio: AudioStorageOption.None,
-      studiedLanguages: ['uk', 'ru'],
-      speechSources: [SpeechSources.WebSpeechAPI],
-      wordMasteryColors: DefaultWordMasteryColors
-    };
-    const expectedSettingsObject: { [key: string]: any }= {};
-    expectedSettingsObject[SettingsManager.settingsKey] = JSON.stringify(expectedSettings as AppSettings);
-    sinon.assert.calledWith(chrome.storage.sync.set, expectedSettingsObject);
-  });
-  it("should load the correct settings from the storage", () => {
-    // force chrome.storage.sync.get to return the correct value
-    const updatedForvoApiKey = 'updatedForvoApiKey';
-    const expectedSettings: ISettings = {
-      lingvoApiKey: '',
-      lingvoApiEnabled: false,
-      forvoApiKey: updatedForvoApiKey,
-      forvoApiEnabled: false,
-      googleApiKey: '',
-      googleApiEnabled: false,
-      preferredVoiceGender: PreferredVoiceGender.Either,
-      primaryLanguage: 'en',
-      primaryLocale: 'en-US',
-      storeAudio: AudioStorageOption.None,
-      studiedLanguages: ['uk', 'ru'],
-      speechSources: [SpeechSources.WebSpeechAPI],
-      wordMasteryColors: DefaultWordMasteryColors
-    };
-    const expectedSettingsObject: { [key: string]: any }= {};
-    expectedSettingsObject[SettingsManager.settingsKey] = JSON.stringify(expectedSettings as AppSettings);
-    chrome.storage.sync.get.yields(expectedSettingsObject);
-
-    const settingsManager = new SettingsManager();
-    expect(settingsManager).toBeTruthy();
-    expect(settingsManager.Settings.forvoApiKey).toBe(updatedForvoApiKey);
+    sinon.assert.calledWith(chrome.storage.sync.set, expectedSettings);
   });
 });
