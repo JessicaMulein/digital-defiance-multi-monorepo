5e6280eac66195afb94096bd862b585965221e28
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-11-13 17:20:56 -0800

    simplify get, update readme

--- digital-defiance/apps/langex/src/app/shared/chromeStorage.ts
+++ digital-defiance/apps/langex/src/app/shared/chromeStorage.ts
@@ -10,15 +10,10 @@ import { IChromeMessage } from "./interfaces";
 
   export function storageGetKey(key: string): any {
     let value: any = null;
-    let count = 0;
     chrome.storage.sync.get(key, (items: { [key: string]: any }) => {
-      count++;
       if (items[key] !== undefined) {
         value = items[key];
       }
     });
-    if (count > 1) {
-      throw new Error('SettingsManager: storageGetKey: count > 1');
-    }
     return value;
   }
