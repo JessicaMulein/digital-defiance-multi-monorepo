83cb88b7aff8f20666c50bdd64b9e36ac51be1f5
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-12-17 22:59:42 -0800

    readme

--- digital-defiance/apps/speak/src/main.ts
+++ digital-defiance/apps/speak/src/main.ts
@@ -18,11 +18,19 @@ getBrowser().then(() => {
 });
 
 // things to do:
+// - configure bluetooth to connect to phone as serial port (https://towardsdatascience.com/sending-data-from-a-raspberry-pi-sensor-unit-over-serial-bluetooth-f9063f3447af)
+//   - when commanded by phone, play audio
+//   - when commanded by phone, list available bluetooth devices to pair to
+//   - when commanded by phone, pair to bluetooth device (then automatically connect to it)
+//   - when commanded by phone, connect to bluetooth paired device
+//   - when commanded by phone, unpair from bluetooth device
+//   - when commanded by phone, switch audio output between local audio port and bluetooth
 // - configure bluetooth to connect to phone as headset 
 //   - configure pulse to route browser audio to bluetooth
 //   - https://raspberrypi.stackexchange.com/questions/76907/pi-as-bluetooth-headset
 //   - https://github.com/lukasjapan/bt-speaker
-// - configure bluetooth to connect to phone as serial port (https://towardsdatascience.com/sending-data-from-a-raspberry-pi-sensor-unit-over-serial-bluetooth-f9063f3447af)
+// - route audio from bluetooth to pulse to either local audio port or headphones connected via bluetooth
+
 // - use either Amazon Polly or Google TTS to generate audio
 //   - Polly: https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/polly-examples.html
 //   - Google TTS: https://cloud.google.com/nodejs/docs/reference/text-to-speech/latest
